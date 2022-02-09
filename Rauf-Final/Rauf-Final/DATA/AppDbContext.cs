0164f0a4504ad69aed8367c4f76067fa7d572b70
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2022-02-09 16:56:59 +0400

    First Added Finalpro

---
+++ Rauf-Final/Rauf-Final/DATA/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Rauf_Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rauf_Final.DATA
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<CustomUser> CustomUsers { get; set; }
+    }
+}
